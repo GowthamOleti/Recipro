69bdead47ed6c7ac6b8dde19da3ba9b27f7ba63b
Teja Karlapudi <teja2495@gmail.com>
2023-06-01 17:29:08 -0500

    basic ui layout

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,86 +1,32 @@
-import React, {PropsWithChildren} from 'react';
+import React from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 export function HomeScreen() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View style={{paddingHorizontal: '10%'}}>
+          <View>
+            <Text style={{marginTop: '20%', fontWeight: '700'}}>
+              Input Text:
+            </Text>
+            <Text> abc </Text>
+            <Text style={{marginTop: '20%', fontWeight: '700'}}>
+              Output Text:
+            </Text>
+            <Text> xyz </Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button title="Summarize" />
+            <Button title="Rewrite" />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -88,20 +34,11 @@ export function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {},
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: '20%',
   },
 });
