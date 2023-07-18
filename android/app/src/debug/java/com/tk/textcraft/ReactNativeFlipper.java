79ab311b97b8cd9e639cc239fe429c093527d824
Teja Karlapudi <teja2495@gmail.com>
2023-07-18 11:54:43 -0500

    update package name

--- android/app/src/debug/java/com/tk/textcraft/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/tk/textcraft/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.textcraft;
+package com.tk.textcraft;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
--- android/app/src/main/java/com/tk/textcraft/MainApplication.java
+++ android/app/src/main/java/com/tk/textcraft/MainApplication.java
@@ -1,4 +1,4 @@
-package com.textcraft;
+package com.tk.textcraft;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
