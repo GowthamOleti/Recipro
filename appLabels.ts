039ae4c7f8d27ff89255ff320b988499bf25dbb1
Teja Karlapudi <teja2495@gmail.com>
2023-07-31 21:19:03 -0500

    update content

--- appLabels.ts
+++ appLabels.ts
@@ -21,15 +21,15 @@ export const appLabels = {
   explainer: {
     about: {
       intro:
-        'TextCraft simplifies reading and writing tasks by using AI. The app currently offers two main features - Summarize and Rewrite.',
+        'TextCraft simplifies the way you read and write by utilizing Al technology. The app currently offers two main features - Summarize and Rewrite.',
       summarizePrefix: '\nSummarize: ',
       summarizeDescription:
         'This feature is useful to summarize lengthy texts or article links. TextCraft can generate a concise and easy-to-understand summary. Long-pressing on summarize will show more options. Also, by enabling the "Auto-Summarize Links" option in settings, you can summarize shared article links instantly.',
       rewritePrefix: '\nRewrite: ',
       rewriteDescription:
-        'TextCraft can also help with writing English content for work or social media. Just give any rough text as input and it will give you a well-written output while preserving the meaning, tone, and essence of the original text. By long-pressing on Rewrite you can access additional options like setting tone, fix grammar, expand and shorten any text.',
+        'TextCraft can also help with writing English content for work or social media. Just give any rough text as input and it will give you a well-written output while preserving the meaning, tone, and essence of the original text. By long-pressing on Rewrite you can access additional options like setting the tone, fixing grammar, expanding and shortening any text.',
       dataCollection:
-        "\nTo keep the app free for everyone, we've provided the option to use your own API Key. We do not send any of your data to our servers. For more information, read our ",
+        "\nNote: To keep the app free for everyone, we've provided the option to use your own API Key. We store this key locally in your device's encrypted storage. Apart from analytics, we do not collect any other data. We use analytics to analyze user behavior and improve the user experience. For information regarding how we handle your data, read our ",
       privacyPolicy: 'Privacy Policy',
       privacyPolicyLink:
         'https://medium.com/@textcraft.app/textcraft-privacy-policy-b1aa01d20c00',
